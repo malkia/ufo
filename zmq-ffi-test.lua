a38ba8525cfdb3e75fb9d86c30b13b8d31a7e7e5
Dimiter "malkia" Stanev <malkia@gmail.com>
2011-04-13 19:18:11 -0700

    Added benchmark. So my ext/ffi is 10 times slower than directly dealing with FFI :) Got to learn lua someday!!!

--- zmq-ffi-test.lua
+++ zmq-ffi-test.lua
@@ -2,26 +2,26 @@ local ffi = require( "ffi" )
 local zmq = require( "ffi/zmq" )
 
 local content = "12345678ABCDEFGH12345678abcdefgh"
+local buf1 = ffi.new( "char[32]" )
+local buf2 = ffi.new( "char[32]" )
 
 local function bounce( sb, sc )
    local rc = zmq.zmq_send( sc, content, 32, 0 )
-   assert( rc == 32 )
+--   assert( rc == 32 )
 
-   local buf1 = ffi.new( "char[32]" )
    rc = zmq.zmq_recv( sb, buf1, 32, 0 )
-   assert( rc == 32 )
+--   assert( rc == 32 )
    rc = zmq.zmq_send( sb, buf1, 32, 0 )
-   assert( rc == 32 )
+--   assert( rc == 32 )
    
-   local buf2 = ffi.new( "char[32]" )
    rc = zmq.zmq_recv( sc, buf2, 32, 0 )
-   assert( rc == 32 )
+--   assert( rc == 32 )
 
-   for i=0, 31 do
-      assert( buf1[i] == buf2[i] )
-   end
+--   for i=0, 31 do
+--      assert( buf1[i] == buf2[i] )
+--   end
 
-   print( ffi.string(buf2,32))
+--   print( ffi.string(buf2,32))
 end
 
 local ctx = zmq.zmq_init (1);
@@ -41,7 +41,9 @@ assert (sc);
 local rc = zmq.zmq_connect (sc, "inproc://a");
 assert (rc == 0);
 
-bounce( sb, sc )    
+for i=0, 1024*1024-1 do
+bounce( sb, sc )   
+end 
 
 local rc = zmq.zmq_close (sc);
 assert (rc == 0);
