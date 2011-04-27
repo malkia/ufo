115cd2e8349a6afec5af548b7e1c53d778f6d83c
Dimiter "malkia" Stanev <malkia@gmail.com>
2011-04-26 18:19:57 -0700

    Scripts for compiling LuaJIT with VS2010, VS2008 and WDK (required for 64-bit)

--- etc/luajit-windows-wdk-build.bat
+++ etc/luajit-windows-wdk-build.bat
@@ -1,11 +1,20 @@
-@echo off
+rem @echo off
 
 setlocal
+
+set _ARCH=%1
+if "%_ARCH%"=="" set _ARCH=32
+
+set _WDK=%2
+if "%_WDK%"=="" set _WDK=d:\wdk
+
 set _LUAJITDIR=%~dp0..\..\luajit\src
-set _OTHERDIR=%~dp0
+set _UFODIR=%~dp0..
 
 set _VSDIR=
-if NOT "%VS90COMNTOOLS%"=="" ( 
+if NOT "%VS100COMNTOOLS%"=="" ( 
+  set _VSDIR="%VS100COMNTOOLS%\..\..\"
+) ELSE IF NOT "%VS90COMNTOOLS%"=="" ( 
   set _VSDIR="%VS90COMNTOOLS%\..\..\"
 ) ELSE IF NOT "%VCINSTALLDIR%"=="" ( 
   set _VSDIR="%VCINSTALLDIR%\..\"
@@ -13,36 +22,40 @@ if NOT "%VS90COMNTOOLS%"=="" (
   set _VSDIR="%VSINSTALLDIR%\"
 ) ELSE (
   echo At least one of the environment variables must be set:
-  echo VS90COMNTOOLS, VCINSTALLDIR or VSINSTALLDIR
+  echo VS100COMNTOOLS, VS90COMNTOOLS, VCINSTALLDIR or VSINSTALLDIR
   goto :EOF
 )
 
-set ARCH=%1
-if "%ARCH%"=="" set ARCH=i386
-if "%ARCH%"=="32" set ARCH=i386
-if "%ARCH%"=="64" set ARCH=amd64
-set ARCH2=%ARCH%
-if "%ARCH%"=="amd64" set ARCH2=x32-64
+if "%_ARCH%"=="32" set _TARGET=i386
+if "%_ARCH%"=="64" set _TARGET=amd64
+if "%_TARGET%"=="" goto :INVALID_ARCH
 
-echo ARCH=%ARCH%
-call d:\wdk\bin\setenv.bat d:\wdk fre %ARCH2% WIN7 no_oacr
-set LIB=%BASEDIR%\lib\crt\%ARCH%;%BASEDIR%\lib\win7\%ARCH%;
+set _CROSS_TARGET=
+if "%_TARGET%"=="amd64" set _CROSS_TARGET=x32-64
+
+echo Compiling LuaJIT: [_ARCH=%_ARCH%] [_TARGET=%_TARGET%] [_CROSS_TARGET=%_CROSS_TARGET%]
+call %_WDK%\bin\setenv.bat %_WDK% fre %_CROSS_TARGET% WIN7 no_oacr
+
+set LIB=%BASEDIR%\lib\crt\%_TARGET%;%BASEDIR%\lib\win7\%_TARGET%;
 set INCLUDE=%CRT_INC_PATH%;%INCLUDE%;
 
 pushd
 cd /d %_LUAJITDIR%
 for %%j in (amalg static) do (
-  call MSVCBUILD-mt.bat %%j
-  for %%i in (exe dll exp lib) do ( 
-    move /y lua51.%%i luajit.%%i
-    if "%ARCH%"=="amd64" ( 
-       move /y luajit.%%i luajit64.%%i
-       echo copy luajit64.%%i %_OTHERDIR%
-       copy luajit64.%%i %_OTHERDIR%
-    ) ELSE (
-       echo copy luajit.%%i %_OTHERDIR%
-       copy luajit.%%i %_OTHERDIR%
-    )
-  )
+  call %~dp0\luajit-windows-msvcbuild-mt-and-shell.bat %%j
+  if "%%j"=="static" call :echo move /y %_LUAJITDIR%\lua51.dll  %_UFODIR%\bin\luajit%_ARCH%.dll
+  if "%%j"=="amalg" call :echo move /y %_LUAJITDIR%\luajit.exe      %_UFODIR%\luajit%_ARCH%.exe
+rem  staticlib echo move /y %_LUAJITDIR%\lua51.lib %_UFODIR%\bin\luajit%_ARCH%.lib
+rem  dynamiclib echo move /y %_LUAJITDIR%\luajit.lib %_UFODIR%\bin\luajit%_ARCH%.lib
 )
 popd
+goto :EOF
+
+:INVALID_ARCH
+echo You should specifiy 32 or 64 as first argument
+goto :EOF
+
+:echo
+echo [%*]
+%*
+goto :EOF
