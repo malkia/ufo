739107d753122f0699a98c92ff2075e76093faf3
Dimiter "malkia" Stanev <malkia@gmail.com>
2011-04-28 07:30:09 -0700

    Added 'git log -1 >> binfile' to zmq/luajit osx builds

--- etc/luajit-osx-build.sh
+++ etc/luajit-osx-build.sh
@@ -1,18 +1,28 @@
-LUAJIT_SRC=../../luajit
-echo Looking for LuaJIT in here LUAJIT_SRC=$LUAJIT_SRC
+LUAJIT=../../luajit
+echo LUAJIT=$LUAJIT
 
-make -C $LUAJIT_SRC clean
-make -C $LUAJIT_SRC "CC=gcc -m32" amalg
-mv $LUAJIT_SRC/src/luajit       luajit32.tmp
-mv $LUAJIT_SRC/src/libluajit.so luajit32.dylib.tmp
+pushd $LUAJIT/src
 
-make -C $LUAJIT_SRC clean
-make -C $LUAJIT_SRC "CC=gcc -m64" amalg
-mv $LUAJIT_SRC/src/luajit       luajit64.tmp
-mv $LUAJIT_SRC/src/libluajit.so luajit64.dylib.tmp
+make clean
+make "CC=gcc -m32" amalg
+mv luajit       luajit32.tmp
+mv libluajit.so luajit32.dylib.tmp
 
-lipo -create -arch i386 luajit32.tmp       -arch x86_64 luajit64.tmp       -output     ../luajit
-lipo -create -arch i386 luajit32.dylib.tmp -arch x86_64 luajit64.dylib.tmp -output ../bin/luajit.dylib
+make clean
+make "CC=gcc -m64" amalg
+mv luajit       luajit64.tmp
+mv libluajit.so luajit64.dylib.tmp
+
+lipo -create -arch i386 luajit32.tmp       -arch x86_64 luajit64.tmp       -output luajit
+lipo -create -arch i386 luajit32.dylib.tmp -arch x86_64 luajit64.dylib.tmp -output luajit.dylib
 
 rm luajit*.tmp
+
+git log -1 >> luajit
+git log -1 >> luajit.dylib
+
+popd
+
+mv $LUAJIT/src/luajit ..
+mv $LUAJIT/src/luajit.dylib ../bin
 
--- etc/zmq-osx-configure.sh
+++ etc/zmq-osx-configure.sh
@@ -1,1 +1,5 @@
+make clean
 ./configure CXX="g++ -arch i386 -arch x86_64" CC="gcc -arch i386 -arch x86_64" CPP=/usr/bin/cpp CXXCPP=/usr/bin/cpp
+make -j8
+cp ./src/.libs/libzmq.dylib ../ufo/bin/zmq.dylib
+git log -1 >> ../ufo/bin/zmq.dylib
