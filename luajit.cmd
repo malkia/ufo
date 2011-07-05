b7ab30c29af6341941c66058961f049dcd9a5998
Dimiter "malkia" Stanev <malkia@gmail.com>
2011-07-05 02:08:48 -0700

    Samples would work now with luajit.cmd, luajit32.cmd and luajit64.cmd if they are started using relative paths, or found in the PATH

--- luajit.cmd
+++ luajit.cmd
@@ -1,3 +1,5 @@
+@SET LUA_PATH=%~dp0\?.lua;%LUA_PATH%
+@SET PATH=%~dp0;%PATH%
 @IF /I "%PROCESSOR_ARCHITECTURE%"=="AMD64" ("%~dpn064.cmd" %*) ELSE ("%~dpn032.cmd" %*)
 
 
--- luajit64.cmd
+++ luajit64.cmd
@@ -1,1 +1,3 @@
+@SET LUA_PATH=%~dp0\?.lua;%LUA_PATH%
+@SET PATH=%~dp0;%PATH%
 @"%~dp0bin\Windows\x64\luajit.exe" %*
