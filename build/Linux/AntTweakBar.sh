ef774d08406826508b821feb1eebf50936a89b7e
Dimiter "malkia" Stanev <malkia@gmail.com>
2011-07-06 01:33:36 -0700

    AntTweakBar linux 32/64-bit, libzmq 64-bit

--- build/Linux/AntTweakBar.sh
+++ build/Linux/AntTweakBar.sh
@@ -1,2 +1,7 @@
-make clean
-make CXX=g++ LINK=g++ LIBS=-lGL
+ROOT=../../../AntTweakBar
+make -C $ROOT/src clean
+make -C $ROOT/src CXX="g++ -m32" LINK="g++ -m32" LIBS=-lGL -j
+cp $ROOT/lib/libAntTweakBar.so ../../bin/Linux/x86
+make -C $ROOT/src clean
+make -C $ROOT/src CXX="g++ -m64" LINK="g++ -m64" LIBS=-lGL -j 
+cp $ROOT/lib/libAntTweakBar.so ../../bin/Linux/x64
