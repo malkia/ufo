ad7d69289168a9992a1d6e70e148fad3b172903b
Dimiter "malkia" Stanev <malkia@gmail.com>
2011-07-05 18:06:54 -0700

    Linux x86/x64 luajit, glfw and samples/scimark.lua from Mike Pall for testing

--- build/Linux/luajit.sh
+++ build/Linux/luajit.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-SRC=../../../luajit/src
-make -C $SRC cleaner
-make -C $SRC -j amalg TARGET_SONAME=libluajit.so BUILDMODE=dynamic
+ROOT=../../../luajit
+make -C $ROOT/src cleaner
+make -C $ROOT/src -j amalg TARGET_SONAME=libluajit.so BUILDMODE=dynamic "CC=gcc -m32"
+mv      $ROOT/src/luajit       ../../bin/Linux/x86
+mv      $ROOT/src/libluajit.so ../../bin/Linux/x86
+make -C $ROOT/src cleaner
+make -C $ROOT/src -j amalg TARGET_SONAME=libluajit.so BUILDMODE=dynamic "CC=gcc -m64"
+mv      $ROOT/src/luajit       ../../bin/Linux/x64
+mv      $ROOT/src/libluajit.so ../../bin/Linux/x64
+
 
