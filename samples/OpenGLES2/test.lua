6c9473845e132bf761822b138572339c91c27bdd
Dimiter "malkia" Stanev <malkia@gmail.com>
2011-07-16 09:30:04 -0700

    Fixed samples/OpenGLES2/test.lua to exit with ESCAPE. Removed some messages

--- samples/OpenGLES2/test.lua
+++ samples/OpenGLES2/test.lua
@@ -11,13 +11,24 @@ local function InitSDL()
    sdl.SDL_GetWMInfo( wminfo )
    local systems = { "win", "x11", "dfb", "cocoa", "uikit" }
    local window = wminfo.info[systems[wminfo.subsystem]].window
+   local event = ffi.new( "SDL_Event" )
    return {
       window = window,
       update = function() 
-		  sdl.SDL_PumpEvents() 
+		  while sdl.SDL_PollEvent( event ) ~= 0 do
+		     if event.type == sdl.SDL_QUIT then
+			return false
+		     end
+		     if event.type == sdl.SDL_KEYUP and event.key.keysym.sym == sdl.SDLK_ESCAPE then
+			event.type = sdl.SDL_QUIT
+			sdl.SDL_PushEvent( event )
+		     end
+		  end
 		  return true 
 	       end,
-      exit = function() sdl.SDL_Quit() end,
+      exit = function() 
+		sdl.SDL_Quit() 
+	     end,
    }
 end
 
@@ -49,17 +60,11 @@ local fs_src = [[
 --      cos( 30.*sqrt(pos.x*pos.x + 1.5*pos.y*pos.y - 1.8*pos.x*pos.y*pos.y)
 --            + atan(pos.y,pos.x) - phase );
 
-print( egl.EGL_DEFAULT_DISPLAY )
-
 local dpy = egl.eglGetDisplay( egl.EGL_DEFAULT_DISPLAY )
-print( 'display', dpy )
-
 local r = egl.eglInitialize( dpy, nil, nil )
-assert( r == egl.EGL_TRUE )
 
 local attr = ffi.new(
-   "EGLint[16]", 
---   egl.EGL_BUFFER_SIZE, 16,
+   "EGLint[3]", 
    egl.EGL_RENDERABLE_TYPE, egl.EGL_OPENGL_ES2_BIT,
    egl.EGL_NONE
 )
@@ -67,19 +72,10 @@ local attr = ffi.new(
 local cfg = ffi.new( "EGLConfig[1]" )
 local n_cfg = ffi.new( "EGLint[1]" )
 local r = egl.eglChooseConfig( dpy, attr, cfg, 1, n_cfg )
-assert( r == egl.EGL_TRUE )
-
-print( string.format("%x", egl.eglGetError() ))
-local surf = egl.eglCreateWindowSurface( dpy, cfg[0], wm.window, nil )
-print( 'surf', surf )
-print( string.format("%x",egl.eglGetError() ))
-
+local surf     = egl.eglCreateWindowSurface( dpy, cfg[0], wm.window, nil )
 local ctx_attr = ffi.new( "EGLint[4]", egl.EGL_CONTEXT_CLIENT_VERSION, 2, egl.EGL_NONE, egl.EGL_NONE )
-local ctx = egl.eglCreateContext( dpy, cfg[0], nil, ctx_attr )
-print( 'context',ctx )
-
+local ctx       = egl.eglCreateContext( dpy, cfg[0], nil, ctx_attr )
 local r = egl.eglMakeCurrent( dpy, surf, surf, ctx )
-print( 'make current', r )
 
 local function print_shader_info( shader )
    local length = ffi.new( "GLint[1]" )
@@ -94,61 +90,36 @@ local function print_shader_info( shader )
    local success = ffi.new( "GLint[1]" )
    gl.glGetShaderiv( shader, gl.GL_COMPILE_STATUS, success )
    local success = success[0]
-   print( 'success', success )
 end
  
 local function load_shader( src, type )
    local shader = gl.glCreateShader( type )
-   local str = ffi.new( "char[?]", #src, src )
-   local ptr = ffi.new( "const char*[?]", str )
-   gl.glShaderSource( shader, 1, ptr, nil )
+   local ptr = ffi.new( "char[?]", #src, src )
+   local ptr2 = ffi.new( "const char*[1]", ptr )
+   gl.glShaderSource( shader, 1, ptr2, nil )
    gl.glCompileShader ( shader );
    print_shader_info ( shader );
    return shader
 end
 
-local function load_shader2( src, type )
-      local shader = gl.glCreateShader( type )
-      print( 'result', string.format("%x", gl.glGetError()))
-      local ptr = ffi.new( "char[?]", #src, src )
-      local ptr2 = ffi.new( "const char*[1]", ptr )
-      gl.glShaderSource( shader, 1, ptr2, nil )
-      gl.glCompileShader ( shader );
-      print_shader_info ( shader );
-      return shader
-end
-
-
-local vs = load_shader2( vs_src, gl.GL_VERTEX_SHADER )
-print( 'vs', vs )
-
-local fs = load_shader2( fs_src, gl.GL_FRAGMENT_SHADER )
-print( 'fs', fs )
+local vs = load_shader( vs_src, gl.GL_VERTEX_SHADER )
+local fs = load_shader( fs_src, gl.GL_FRAGMENT_SHADER )
 
 local prog
 local loc_position
 local loc_phase   
 local loc_offset  
 
-local function CHK()
-   local e = gl.glGetError()
-   print( 'R', e )
-end
-
 if vs ~= nil and fs ~= nil then
    prog = gl.glCreateProgram()
-   print( 'prog', prog )
-   
-   CHK(gl.glAttachShader( prog, vs ))
-   CHK(gl.glAttachShader( prog, fs ))
-   CHK(gl.glLinkProgram( prog ))
-   CHK(gl.glUseProgram( prog ))
+   gl.glAttachShader( prog, vs )
+   gl.glAttachShader( prog, fs )
+   gl.glLinkProgram( prog )
+   gl.glUseProgram( prog )
    
    loc_position = gl.glGetAttribLocation( prog, "position" )
    loc_phase    = gl.glGetUniformLocation( prog, "phase" )
    loc_offset   = gl.glGetUniformLocation( prog, "offset" )
-   
-   print( loc_position, loc_phase, loc_offset )
 end
 
 local ww, wh = 640, 480
@@ -175,10 +146,8 @@ local vertexArray = ffi.new(
 )
 
 while wm:update() do
---   print( 'frames', frames )
    frames = frames + 1
 
---   print(3) 
    gl.glViewport( 0, 0, ww, wh )
    gl.glClearColor( 0.08, 0.06, math.random()/4, 1)
    gl.glClear ( gl.GL_COLOR_BUFFER_BIT )
