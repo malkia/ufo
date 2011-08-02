996dfd545be1ae889d37e32f98d1b5ec1262f3fa
Dimiter 'malkia' Stanev <malkia@gmail.com>
2011-08-02 10:41:59 -0700

    zlib 1.2.5 for windows

---
+++ build/Windows/zlib.cmd
@@ -0,0 +1,32 @@
+@echo off
+call %~dp0/wdk/setup %~n0 %*
+pushd %LB_PROJECT_ROOT%
+setlocal
+
+rem ZLIB usually comes precompiled as tarball
+rem I put it at ../../../zlib, and did this in it:
+rem git init
+rem git add *
+rem git commit -a -m "initial"
+rem
+rem Now git is used to clean object files
+rem
+git clean -fdx
+
+if "%LB_TARGET_ARCH%"=="x86" set OBJA=inffas32.obj match686.obj
+if "%LB_TARGET_ARCH%"=="x86" set AS=ml
+if "%LB_TARGET_ARCH%"=="x64" set OBJA=inffasx64.obj gvmat64.obj inffas8664.obj
+if "%LB_TARGET_ARCH%"=="x64" set AS=ml64
+
+nmake -f win32/Makefile.msc CC="cl -I." OBJA="%OBJA%" AS="%AS%" LOC="-DASMV -DASMINF" AR="link /LIB" LD="link /RELEASE /SWAPRUN:NET /SWAPRUN:CD %LB_OBJS%"
+
+set NAME=%LB_PROJECT_NAME%
+
+move /y zlib1.dll %NAME%.dll
+move /y zdll.lib %NAME%.lib
+
+call %~dp0/wdk/install %NAME%.dll
+call %~dp0/wdk/install %NAME%.lib
+endlocal
+popd
+
