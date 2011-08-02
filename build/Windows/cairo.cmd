45a7d23607bf15e083d8f474e9dfde2f0d37328b
Dimiter 'malkia' Stanev <malkia@gmail.com>
2011-08-01 21:24:11 -0700

    Fixed cairo for windows

--- build/Windows/cairo.cmd
+++ build/Windows/cairo.cmd
@@ -9,7 +9,7 @@ copy /y %~dpn0.features build\Makefile.win32.features
 copy /y ..\pixman\pixman.%LB_TARGET_ARCH%.lib ..\pixman\pixman\release\pixman-1.lib
 make -f Makefile.win32 CFG=release AR="link /LIB" LD="link /RELEASE /SWAPRUN:NET /SWAPRUN:CD %LB_OBJS%"
 
-call %~dp0/wdk/install %LB_PROJECT_NAME%.dll
+call %~dp0/wdk/install src\release\%LB_PROJECT_NAME%.dll
 endlocal
 popd
 
