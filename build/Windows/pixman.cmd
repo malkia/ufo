6979280725a393d47beb66bb582511ac25265d5f
Dimiter 'malkia' Stanev <malkia@gmail.com>
2011-08-04 22:41:34 -0700

    Disabled pixman's Thread Local Storage, as it's not supported on XP (For Real :))

--- build/Windows/pixman.cmd
+++ build/Windows/pixman.cmd
@@ -24,7 +24,7 @@ sed -e "s/@PIXMAN_VERSION_MAJOR@/%VERSION_MAJOR%/g"^
      < pixman/pixman-version.h.in^
      > pixman/pixman-version.h
 
-make MMX=%MMX% CC="-DPIXMAN_NO_TLS=1 cl %LB_CL_ARCH_SSE2% /GL /O2 /Ob2 /Ox /Oi /Ot /Oy /GF /Zi /Fd../%NAME%.pdb /wd4005" -C pixman -f Makefile.win32 CFG=release
+make MMX=%MMX% CC="cl -DPIXMAN_NO_TLS=1 %LB_CL_ARCH_SSE2% /GL /O2 /Ob2 /Ox /Oi /Ot /Oy /GF /Zi /Fd../%NAME%.pdb /wd4005" -C pixman -f Makefile.win32 CFG=release
 echo EXPORTS > %NAME%.def
 link /DUMP /NOLOGO /LINKERMEMBER:1 %NAME%/release/%NAME%-1.lib | grep -E " [ 0-9A-Z]{7}[0-9A-Z] [A-Za-z_]+" | cut -b%CUTSYMPOS%- | sort | uniq >> %NAME%.def
 link /LTCG /NOLOGO /MACHINE:%LB_TARGET_ARCH% /OUT:%NAME%.dll /PDB:%NAME%.pdb /PDBCOMPRESS /DEF:%NAME%.def /DLL /DEBUG /OPT:REF /OPT:ICF=1000 /SWAPRUN:NET,CD %LB_OBJS% %NAME%/release/%NAME%-1.lib
