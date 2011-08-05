b3aab8ea4a392e19289b8d8bf54c90213462651b
Dimiter 'malkia' Stanev <malkia@gmail.com>
2011-08-05 04:13:00 -0700

    More work on getting all PDB's in

--- build/Windows/luajit.cmd
+++ build/Windows/luajit.cmd
@@ -1,17 +1,30 @@
 @echo off
+set LB_LINK_SWAPRUN=-NOLOGO
 call %~dp0/wdk/setup %~n0 %*
 pushd %LB_PROJECT_ROOT%\src
 setlocal
 
 git clean -fdx
 
-echo cl.exe /D_MSC_VER=1399 /DLUAJIT_ENABLE_LUA52COMPAT=1 %%* > cl.cmd
+echo @echo off > cl.cmd
+echo set PDB= >> cl.cmd
+echo if exist buildvm.exe set PDB=/Fdlua51.pdb >> cl.cmd
+echo if exist lua51.dll set PDB=/Fdluajit.pdb >> cl.cmd
+echo cl.exe %LB_CL_OPTS% /D_MSC_VER=1399 /DLUAJIT_ENABLE_LUA52COMPAT=1 %%* %%PDB%% >> cl.cmd
+
 rem LOL! /SWAPRUN:NET,CD makes Symantec AntiVir go crazy :)
-echo link.exe /RELEASE %LB_OBJS% %%* > link.cmd
+
+echo @echo off > link.cmd
+echo set PDB= >> link.cmd
+echo if exist buildvm.exe set PDB=-PDB:lua51.pdb >> link.cmd
+echo if exist lua51.dll set PDB=-PDB:luajit.pdb >> link.cmd
+echo link.exe /DEBUG %LB_OBJS% %%* %%PDB%% >> link.cmd
 call msvcbuild amalg
 
 call %~dp0/wdk/install lua51.dll
 call %~dp0/wdk/install lua51.lib
+call %~dp0/wdk/install lua51.pdb
 call %~dp0/wdk/install luajit.exe
+call %~dp0/wdk/install luajit.pdb
 endlocal
 popd
