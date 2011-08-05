765071349073ca1fd80995efb0e6b3c2a2ef9df8
Dimiter "malkia" Stanev <malkia@gmail.com>
2011-08-05 10:27:47 -0700

    Working on the SDL Windows Manager

--- lib/wm/sdl.lua
+++ lib/wm/sdl.lua
@@ -16,15 +16,11 @@ function wm:init(width, height)
    sdl.SDL_PushEvent( self.event )
    self.mx, self.my, self.mb = 0, 0, {}
    self.kb, self.km = 0, 0
-   self.running = true
 end
 
 function wm:exit()
-   for k,_ in pairs(self) do
-      self[k] = nil
-   end
-   sdl.SDL_Quit()
-   self.update = function() return false end
+   self.event.type = sdl.SDL_QUIT
+   sdl.SDL_PushEvent(self.event)
 end
 
 local function handle( type, handlers )
@@ -37,32 +33,52 @@ function wm:update()
    if self.window == nil then
       self:init()
    end
-   if not self.running then
-      return
-   end
    while sdl.SDL_PollEvent( self.event ) ~= 0 do
       local ks, mm, bn = self.event.key.keysym, self.event.motion, self.event.button.button
       handle(
 	 self.event.type, {
-	    [sdl.SDL_QUIT]            = function() wm:exit(); self = nil             end,
+	    [sdl.SDL_QUIT] =
+	       function()
+		  if wm.exiting then
+		     wm:exiting()
+		  end
+		  sdl.SDL_Quit()
+		  if wm.exited then
+		     wm:exited()
+		  end
+		  for k,_ in pairs(self) do
+		     self[k] = nil
+		  end
+		  self.update = function() return false end
+		  return false
+	       end,
 	    [sdl.SDL_MOUSEMOTION]     = function() self.mx, self.my = mm.x, mm.y     end,
 	    [sdl.SDL_MOUSEBUTTONDOWN] = function() self.mb[ bn ] = true              end,
 	    [sdl.SDL_MOUSEBUTTONUP]   = function() self.mb[ bn ] = false             end,
-	    [sdl.SDL_KEYDOWN]         = function() self.kb, self.km = ks.sym, ks.mod end,
+	    [sdl.SDL_KEYDOWN] =
+	       function()
+		  self.kb, self.km = ks.sym, ks.mod
+		  if self.key_pressed then
+		     self:key_pressed()
+		  end
+	       end,
 	    [sdl.SDL_VIDEORESIZE] =
-	    function()
-	       self.width, self.height = self.event.resize.w, self.event.resize.h
-	       self.window = sdl.SDL_SetVideoMode(
-		  self.width, self.height, 32,
-		  bit.bor(sdl.SDL_DOUBLEBUF, sdl.SDL_RESIZABLE)
-	       )
-	       return true
-	    end,
+	       function()
+		  self.width, self.height = self.event.resize.w, self.event.resize.h
+		  if self.resizing then
+		     self:resizing()
+		  end
+		  self.window = sdl.SDL_SetVideoMode(
+		     self.width, self.height, 32,
+		     bit.bor(sdl.SDL_DOUBLEBUF, sdl.SDL_RESIZABLE)
+		  )
+		  if self.resized then
+		     self:resized()
+		  end
+		  return true
+	       end,
 
       })
-      if not self then
-	 return false
-      end
    end
    sdl.SDL_Flip( self.window )
    return true
@@ -73,8 +89,27 @@ if ... then
 end
 
 do
+   function wm:resizing()
+      print( 'Resizing to', self.width, self.height )
+   end
+   function wm:resized()
+      print( 'Resized to', self.width, self.height )
+   end
+   function wm:exiting()
+      print( 'Exiting' )
+   end
+   function wm:exited()
+      print( 'Exited' )
+   end
+   function wm:key_pressed()
+      print( 'Key pressed', wm.kb )
+      if wm.kb == 27 then
+	 wm:exit()
+      end
+   end
    while wm:update() do
-      if wm.kb == 27 then 
+      if wm.kb == 32 then
+	 -- polling exit
 	 wm:exit()
       end
    end
--- samples/cairo/sdl.lua
+++ samples/cairo/sdl.lua
@@ -1,97 +1,58 @@
 local ffi = require( "ffi" )
 local sdl = require( "ffi/SDL" )
 local cr  = require( "ffi/cairo" )
+local wm  = require( "lib/wm/sdl" )
 local random = math.random
 
-local function handle( type, handlers )
-   if handlers[type] then
-      return handlers[type]()
-   end
-end
-
-local terminal = {
-   lines = {},
-   top   = 0,
-   left  = 0,
-   line  = 1,
-   col   = 1,
-   font_extents = ffi.new( "cairo_font_extents_t" )
-}
-
-function terminal:update( cr_ctx, terminal )
-   cr.cairo_save( cr_ctx )
-   cr.cairo_font_extents( cr_ctx, self.font_extents )
-   local srf = cr.cairo_get_target( cr_ctx )
-   cr.cairo_restore( cr_ctx )
-end
-
 do
-   local cr_srf, cr_obj
-   local sw, sh = 640, 480
-   local mx, my, mb, kb, km = 0, 0, {}, 0, 0
-   local running, evt = true, ffi.new( "SDL_Event" )
-   local screen = sdl.SDL_Init(0xFFFF)
-   evt.type, evt.resize.w, evt.resize.h = sdl.SDL_VIDEORESIZE, sw, sh
-   sdl.SDL_PushEvent( evt )
-   while running do
-      while sdl.SDL_PollEvent( evt ) > 0 do
-	 local ks, mm, bn = evt.key.keysym, evt.motion, evt.button.button
- 	 handle (
-	    evt.type, {
-	       [sdl.SDL_QUIT]            = function() running = false           end,
-	       [sdl.SDL_MOUSEMOTION]     = function() mx, my   = mm.x, mm.y     end,
-	       [sdl.SDL_MOUSEBUTTONDOWN] = function() mb[ bn ] = true           end,
-	       [sdl.SDL_MOUSEBUTTONUP]   = function() mb[ bn ] = false          end,
-	       [sdl.SDL_KEYDOWN]         = function() kb, km   = ks.sym, ks.mod end,
-	       [sdl.SDL_KEYUP]           = function() running  = (kb ~= sdl.SDLK_ESCAPE) end,
-	       [sdl.SDL_VIDEORESIZE]     =
-		  function()
-		     sw, sh = evt.resize.w, evt.resize.h
-		     screen = sdl.SDL_SetVideoMode(
-			sw, sh, 32,
-			bit.bor(sdl.SDL_DOUBLEBUF, sdl.SDL_RESIZABLE)
-		     )
-		     cr_srf = ffi.gc(
-			cr.cairo_image_surface_create( cr.CAIRO_FORMAT_ARGB32, sw, sh ),
-			cr.cairo_surface_destroy
-		     )
-		     cr_ctx = ffi.gc(
-			cr.cairo_create( cr_srf ),
-			cr.cairo_destroy
-		     )
-		     sdl_srf = ffi.gc(
-			sdl.SDL_CreateRGBSurfaceFrom(
-			   cr.cairo_image_surface_get_data( cr_srf ),
-			   sw, sh, 32, cr.cairo_format_stride_for_width( cr.CAIRO_FORMAT_ARGB32, sw ),
-			    0,  0,  0,  0
-			),
-			SDL_FreeSurface
-		     )
-		     sdl.SDL_WM_SetCaption( "Cairo Testing", nil )
-		  end,
-	    })
-      end
+   local surf, ctx, sdl_surf
 
-      cr.cairo_set_antialias(      cr_ctx, cr.CAIRO_ANTIALIAS_GRAY )
-      cr.cairo_save(               cr_ctx )
-      cr.cairo_set_operator(       cr_ctx, cr.CAIRO_OPERATOR_CLEAR )
-      cr.cairo_paint(              cr_ctx )
-      cr.cairo_restore(            cr_ctx )
-      cr.cairo_set_source_rgba(    cr_ctx, random(), random(), random(), random() )
-      cr.cairo_select_font_face(   cr_ctx, "Lucida Console", 0, 0 )
-      cr.cairo_set_font_size(      cr_ctx, 20 )
-      cr.cairo_move_to(            cr_ctx, mx, my )
-      cr.cairo_show_text(          cr_ctx, "Some text" )
-      cr.cairo_move_to(            cr_ctx, sw/2, sh/2 )
-      for i=1,10 do
-	 cr.cairo_set_line_width(  cr_ctx, random(i))
-	 cr.cairo_set_source_rgba( cr_ctx, random(), random(), random(), random() )
-	 cr.cairo_line_to(         cr_ctx, random(mx, my), random(sw-mx, sh-my))
-      end
-      cr.cairo_stroke(             cr_ctx )
-
-      sdl.SDL_UpperBlit(           sdl_srf, nil, screen, nil )
-      sdl.SDL_Flip(                screen )
+   function wm:resized()
+      surf = ffi.gc(
+	 cr.cairo_image_surface_create( cr.CAIRO_FORMAT_ARGB32, self.width, self.height ),
+	 cr.cairo_surface_destroy
+      )
+      ctx = ffi.gc(
+	 cr.cairo_create( surf ),
+	 cr.cairo_destroy
+      )
+      sdl_surf = ffi.gc(
+	 sdl.SDL_CreateRGBSurfaceFrom(
+	    cr.cairo_image_surface_get_data( surf ),
+	    self.width, self.height, 32,
+	    cr.cairo_format_stride_for_width( cr.CAIRO_FORMAT_ARGB32, self.width ),
+	    0,  0,  0,  0
+	 ),
+	 SDL_FreeSurface
+      )
+      sdl.SDL_WM_SetCaption( "Cairo Testing", nil )
    end
-   sdl.SDL_Quit()
+
+   function wm:key_pressed()
+      if self.kb == 27 then
+	 wm:exit()
+      end
+   end
+
+   while wm:update() do
+      cr.cairo_set_antialias(      ctx, cr.CAIRO_ANTIALIAS_GRAY )
+      cr.cairo_save(               ctx )
+      cr.cairo_set_operator(       ctx, cr.CAIRO_OPERATOR_CLEAR )
+      cr.cairo_paint(              ctx )
+      cr.cairo_restore(            ctx )
+      cr.cairo_set_source_rgba(    ctx, random(), random(), random(), random() )
+      cr.cairo_select_font_face(   ctx, "Lucida Console", 0, 0 )
+      cr.cairo_set_font_size(      ctx, 20 )
+      cr.cairo_move_to(            ctx, wm.mx, wm.my )
+      cr.cairo_show_text(          ctx, "Some text" )
+      cr.cairo_move_to(            ctx, wm.width/2, wm.height/2 )
+      for i=1,10 do
+	 cr.cairo_set_line_width(  ctx, random(i))
+	 cr.cairo_set_source_rgba( ctx, random(), random(), random(), random() )
+	 cr.cairo_line_to(         ctx, random(wm.mx, wm.my),
+				   random(wm.width-wm.mx, wm.height-wm.my))
+      end
+      cr.cairo_stroke(             ctx )
+      sdl.SDL_UpperBlit(           sdl_surf, nil, wm.window, nil )
+   end
 end
