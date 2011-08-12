1c42261705ae9f7c8a64a89522c106b932e469b0
Dimiter 'malkia' Stanev <malkia@gmail.com>
2011-08-11 22:57:11 -0700

    LuaJIT latest windows

--- build/Windows/luajit.cmd
+++ build/Windows/luajit.cmd
@@ -1,4 +1,4 @@
-@echo off
+rem @echo off
 set LB_LINK_SWAPRUN=-NOLOGO
 call %~dp0/wdk/setup %~n0 %*
 pushd %LB_PROJECT_ROOT%\src
@@ -7,6 +7,7 @@ setlocal
 git clean -fdx
 
 echo @echo off > %~dp0/bin/cl.cmd
+echo echo ....... Custom cl.cmd [%%*] >> %~dp0/bin/cl.cmd
 echo set PDB= >> %~dp0/bin/cl.cmd
 echo if exist buildvm.exe set PDB=/Fdlua51.pdb >> %~dp0/bin/cl.cmd
 echo if exist lua51.dll set PDB=/Fdluajit.pdb >> %~dp0/bin/cl.cmd
@@ -15,6 +16,7 @@ echo cl.exe %LB_CL_OPTS% /D_MSC_VER=1399 /DLUAJIT_ENABLE_LUA52COMPAT=1 %%* %%PDB
 rem LOL! /SWAPRUN:NET,CD makes Symantec AntiVir go crazy :)
 
 echo @echo off > %~dp0/bin/link.cmd
+echo echo ....... Custom link.cmd [%%*] >> %~dp0/bin/link.cmd
 echo set PDB= >> %~dp0/bin/link.cmd
 echo if exist buildvm.exe set PDB=-PDB:lua51.pdb >> %~dp0/bin/link.cmd
 echo if exist lua51.dll set PDB=-PDB:luajit.pdb >> %~dp0/bin/link.cmd
@@ -31,9 +33,10 @@ rem static version of the lua51.lib library,
 rem renamed to luajit.lib
 rem as lua51.lib already is the dll import library for lua51.dll
 rem
-del link.cmd
+del %~dp0\bin\link.cmd
 call msvcbuild static
 echo @echo off > %~dp0/bin/cl.cmd
+echo echo ....... Custom cl.cmd [%%*] >> %~dp0/bin/cl.cmd
 echo cl.exe %LB_CL_OPTS% /D_MSC_VER=1399 /DLUAJIT_ENABLE_LUA52COMPAT=1 %%* >> %~dp0/bin/cl.cmd
 call %~dp0/wdk/install lua51.lib lua51_static.lib
 
