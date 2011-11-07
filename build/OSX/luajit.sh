a66fad785f5f349e3f7f37e07aee212c1ffc7842
Dimiter "malkia" Stanev <malkia@gmail.com>
2011-11-06 23:15:16 -0800

    Moved libluajit.dylib -> luajit.dylib

--- build/OSX/luajit.sh
+++ build/OSX/luajit.sh
@@ -1,19 +1,22 @@
+set -e
 LUAJIT=../../../luajit
-rm *.tmp 1>/dev/null 2>/dev/null
-make -C $LUAJIT -j BUILDMODE=dynamic CC="gcc -mmacosx-version-min=10.4 -arch i386"   LUAJIT_SO=libluajit.dylib TARGET_DYLIBPATH=libluajit.dylib cleaner
-make -C $LUAJIT -j BUILDMODE=dynamic CC="gcc -mmacosx-version-min=10.4 -arch i386"   LUAJIT_SO=libluajit.dylib TARGET_DYLIBPATH=libluajit.dylib amalg
-mv $LUAJIT/src/luajit luajit32.tmp
-mv $LUAJIT/src/libluajit.dylib libluajit32.dylib.tmp
-make -C $LUAJIT -j BUILDMODE=dynamic CC="gcc -mmacosx-version-min=10.4 -arch x86_64" LUAJIT_SO=libluajit.dylib TARGET_DYLIBPATH=libluajit.dylib cleaner
-make -C $LUAJIT -j BUILDMODE=dynamic CC="gcc -mmacosx-version-min=10.4 -arch x86_64" LUAJIT_SO=libluajit.dylib TARGET_DYLIBPATH=libluajit.dylib amalg
-mv $LUAJIT/src/luajit luajit64.tmp
-mv $LUAJIT/src/libluajit.dylib libluajit64.dylib.tmp
-lipo -create ./luajit*.tmp -output luajit
-lipo -create ./libluajit*.dylib.tmp -output libluajit.dylib
+(rm *.tmp 1>/dev/null 2>/dev/null) && true
+make -C $LUAJIT -j BUILDMODE=dynamic CC="gcc -mmacosx-version-min=10.5 -arch i386"   LUAJIT_SO=luajit.dylib TARGET_DYLIBPATH=luajit.dylib cleaner
+make -C $LUAJIT -j BUILDMODE=dynamic CC="gcc -mmacosx-version-min=10.5 -arch i386"   LUAJIT_SO=luajit.dylib TARGET_DYLIBPATH=luajit.dylib amalg
+mv $LUAJIT/src/luajit luajit32.exe.tmp
+mv $LUAJIT/src/luajit.dylib luajit32.dylib.tmp
+make -C $LUAJIT -j BUILDMODE=dynamic CC="gcc -mmacosx-version-min=10.5 -arch x86_64" LUAJIT_SO=luajit.dylib TARGET_DYLIBPATH=luajit.dylib cleaner
+make -C $LUAJIT -j BUILDMODE=dynamic CC="gcc -mmacosx-version-min=10.5 -arch x86_64" LUAJIT_SO=luajit.dylib TARGET_DYLIBPATH=luajit.dylib amalg
+mv $LUAJIT/src/luajit luajit64.exe.tmp
+mv $LUAJIT/src/luajit.dylib luajit64.dylib.tmp
+lipo -create ./luajit*.exe.tmp -output luajit
+lipo -create ./luajit*.dylib.tmp -output luajit.dylib
 # rm *.tmp 1>/dev/null 2>/dev/null
-install_name_tool -id @rpath/libluajit.dylib libluajit.dylib
-install_name_tool -change libluajit.dylib @executable_path/libluajit.dylib luajit
+install_name_tool -id @rpath/luajit.dylib luajit.dylib
+install_name_tool -change luajit.dylib @executable_path/luajit.dylib luajit
 git --git-dir=$LUAJIT/.git log -1 >> luajit
-git --git-dir=$LUAJIT/.git log -1 >> libluajit.dylib
+git --git-dir=$LUAJIT/.git log -1 >> luajit.dylib
 mv luajit ../../bin/OSX
-mv libluajit.dylib ../../bin/OSX
+mv luajit.dylib ../../bin/OSX/luajit.dylib
+echo Done
+
