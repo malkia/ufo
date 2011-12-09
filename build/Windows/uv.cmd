e3bc3178364c8584a2f8d605ea834e6be7b32823
Dimiter 'malkia' Stanev <malkia@gmail.com>
2011-12-09 10:43:46 -0800

    Windows: Added libuv (git://github.com/joyent/libuv.git). It seems that it includes ares too. Still no ffi bindings for it

---
+++ build/Windows/uv.cmd
@@ -0,0 +1,24 @@
+@echo off
+set LB_PROJECT_REPO=libuv
+call %~dp0/wdk/setup %~n0 %*
+setlocal
+pushd %LB_PROJECT_ROOT%
+git clean -fdx
+
+set NAME=%LB_PROJECT_NAME%
+
+echo src/uv-common.c>sources.tmp
+for %%i in (src\ares\*.c) do echo %%i >> sources.tmp
+for %%i in (src\win\*.c) do echo %%i >> sources.tmp
+
+cl %LB_CL_OPTS% -EHsc -Fe%NAME%.dll -LD^
+ -DHAVE_CONFIG_H -D_WIN32_WINNT=0x0502 -DEIO_STACKSIZE=262144 -D_GNU_SOURCE -DWIN32 -DNDEBUG^
+ -DBUILDING_UV_SHARED^
+ -Iinclude -Iinclude/uv-private -Isrc -Isrc/ares/config_win32^
+ -I"%~dp0\include" @sources.tmp /link"%LB_LINK_OPTS% ws2_32.lib advapi32.lib"
+
+call %~dp0/wdk/install %LB_PROJECT_NAME%.dll
+call %~dp0/wdk/install %LB_PROJECT_NAME%.lib
+call %~dp0/wdk/install %LB_PROJECT_NAME%.pdb
+endlocal
+popd
