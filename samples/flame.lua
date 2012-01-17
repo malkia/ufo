620ed8b1fb00a11202cdfcf017596a7c5a6bf3af
Dimiter 'malkia' Stanev <malkia@gmail.com>
2012-01-16 21:01:07 -0800

    Cleaned up a little bit Dave's samples/flame.lua

--- samples/flame.lua
+++ samples/flame.lua
@@ -12,21 +12,6 @@ local ffi = require( "ffi" )
 local sdl = require( "ffi/sdl" )
 local uint32ptr = ffi.typeof( "uint32_t*" )
 
-ffi.cdef[[
-void Sleep(int ms);
-int poll(struct pollfd *fds, unsigned long nfds, int timeout);
-]]
-local sleep
-if ffi.os == "Windows" then
-  function sleep(s)
-    ffi.C.Sleep(s*1000)
-  end
-else
-  function sleep(s)
-    ffi.C.poll(nil, 0, s*1000)
-  end
-end
-
 -- HELPERS
 --
 local bit = require'bit'
