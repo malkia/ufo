a86bbd2e4f55422224ba2f71b0e326a3945438ee
Dimiter 'malkia' Stanev <malkia@gmail.com>
2012-01-16 21:04:05 -0800

    Cleaned up the png.cmd build script

--- build/Windows/png.cmd
+++ build/Windows/png.cmd
@@ -12,7 +12,6 @@ if "%LB_TARGET_BITS%"=="32" set CUTSYMPOS=12
 if "%LB_TARGET_BITS%"=="64" set CUTSYMPOS=11
 
 echo EXPORTS > %NAME%.def
-rem link /DUMP /symbols /exports %NAME%_static.lib | grep " notype ()    External " | grep " SECT"  | cut -b%CUTSYMPOS%- | sort | uniq >> %NAME%.def
 link /DUMP /NOLOGO /LINKERMEMBER:1 %NAME%_static.lib | grep -E " [ 0-9A-Z]{7}[0-9A-Z] [A-Za-z_]+" | cut -b%CUTSYMPOS%- | sort | uniq >> %NAME%.def
 link %LB_LINK_OPTS% /OUT:%NAME%.dll /DEF:%NAME%.def %NAME%_static.lib %LB_ROOT%\bin\Windows\%LB_TARGET_ARCH%\z.lib
 move %LB_PROJECT_NAME%.pdb %NAME%.pdb
