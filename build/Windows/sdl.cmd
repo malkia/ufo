ca31e443fc2aa0655cb9067e5a94cdf2d0d80daf
Dimiter 'malkia' Stanev <malkia@gmail.com>
2012-02-16 00:34:43 -0800

    Moved back to SDL-1.3

--- build/Windows/sdl.cmd
+++ build/Windows/sdl.cmd
@@ -3,17 +3,15 @@ call %~dp0/wdk/setup %~n0 %*
 pushd %LB_PROJECT_ROOT%\VisualC\SDL
 setlocal
 
-call hg purge --all
+rem SDL-1.3 :)
+rem hg update 7fe4b2b78acc
 
 set NAME=%LB_PROJECT_NAME%
-rem don't forget to "hg checkout SDL-1.2"
+copy ..\..\include\SDL_config_win32.h ..\..\include\SDL_config.h
 
-copy ..\..\include\SDL_config_win32.h ..\..\include\SDL_config.h 1>nul 2>nul
+(for /f "usebackq tokens=1,2,3* delims==" %%i in (`findstr RelativePath %NAME%_VS2008.vcproj`) do if /I "%%~xj"==".c" if /I NOT "%%~nxj"=="SDL_stdlib.c" echo %%j) > sources.tmp
 
-del sources.tmp 1>nul 2>nul
-(for /f "usebackq tokens=1,2,3* delims==" %%i in (`findstr RelativePath %NAME%.vcproj`) do if /I "%%~xj"==".c" echo %%j) > sources.tmp
-
-cl %LB_CL_OPTS% -LD -Fe%NAME%.dll -DNDEBUG=1 -D_WINDOWS -I"%DXSDK_DIR%\include" -I..\..\include -I"%~dp0include" @sources.tmp /link"%LB_LINK_OPTS% user32.lib gdi32.lib kernel32.lib winmm.lib imm32.lib ole32.lib version.lib oleaut32.lib dxguid.lib advapi32.lib"
+cl %LB_CL_OPTS% -Z7 -MP -LD -Fe%NAME%.dll -DNDEBUG=1 -D_WINDOWS -I"%DXSDK_DIR%\include" -I..\..\include -I"%~dp0include" @sources.tmp /link"%LB_LINK_OPTS% user32.lib gdi32.lib kernel32.lib winmm.lib imm32.lib ole32.lib version.lib oleaut32.lib Advapi32.lib dxguid.lib"
 
 call %~dp0/wdk/install %NAME%.dll
 call %~dp0/wdk/install %NAME%.lib
@@ -23,3 +21,7 @@ endlocal
 popd
 goto :eof
 
+:addsrc
+for %%k in (%SRCFILTER%) do if /I "%%k"=="%1" goto :EOF
+echo %1 >> sources.tmp
+goto :eof
