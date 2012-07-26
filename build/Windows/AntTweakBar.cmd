69709c221cb40d8259cefe147f43e46ab704d662
Dimiter 'malkia' Stanev <malkia@gmail.com>
2012-07-25 22:14:07 -0700

    Updated AntTweakBar to 1.15 (Windows)

--- build/Windows/AntTweakBar.cmd
+++ build/Windows/AntTweakBar.cmd
@@ -14,7 +14,8 @@ mkdir release%LB_TARGET_BITS% 1>nul 2>nul
 "%DXSDK_DIR%\Utilities\bin\x86\fxc" /T ps_4_0_level_9_1 /E TextPS /Fh             release%LB_TARGET_BITS%\TwDirect3D11_TextPS.h             TwDirect3D11.hlsl
 
 cl -LD -Fe%NAME%.dll %LB_CL_OPTS% -I"%DXSDK_DIR%/Include" -DTW_EXPORTS=1 -I../include^
-   LoadOGL.cpp LoadOGLCore.cpp TwBar.cpp TwColors.cpp TwEventSFML.cpp TwFonts.cpp TwMgr.cpp TwOpenGL.cpp TwDirect3D9.cpp TwDirect3D10.cpp TwDirect3D11.cpp^
+   LoadOGL.cpp LoadOGLCore.cpp TwBar.cpp TwColors.cpp TwEventSFML.cpp TwFonts.cpp TwMgr.cpp^
+   TwOpenGL.cpp TwOpenGLCore.cpp TwDirect3D9.cpp TwDirect3D10.cpp TwDirect3D11.cpp^
    TwEventSDL.c TwEventSDL12.c TwEventSDL13.c^
    /link"%LB_LINK_OPTS% user32.lib gdi32.lib kernel32.lib"
 
