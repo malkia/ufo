5c51f1c6423c3413b2614c1e49cd7336a60037f9
Dimiter 'malkia' Stanev <malkia@gmail.com>
2012-07-25 22:31:56 -0700

    Updated luajit e8af6e9da4465ffb02f89bee8dfc372b48741bf7 (Android - arm/mips/x86, ndk-r8b, gcc 4.6)

--- build/Android/luajit.cmd
+++ build/Android/luajit.cmd
@@ -1,21 +1,42 @@
 @echo off
 pushd "%~dp0"
 setlocal
+git clean -fdx
 
-for /f %%i in ('cygpath -m ../../../android-ndk-r6') do set NDK=%%i
+for /f %%i in ('cygpath -m ../../../android-ndk-r8b') do set NDK=%%i
 
-set NDKVER=%NDK%/toolchains/arm-linux-androideabi-4.4.3
-set NDKP=%NDKVER%/prebuilt/windows/bin/arm-linux-androideabi-
+set NDKARCH=%1
+if "%NDKARCH%"=="" set NDKARCH=arm
 
-set NDKABI=8
-set NDKF=--sysroot %NDK%/platforms/android-%NDKABI%/arch-arm
+if /I "%NDKARCH%"=="arm"  set NDKTRIPLE1=arm-linux-androideabi
+if /I "%NDKARCH%"=="arm"  set NDKTRIPLE2=arm-linux-androideabi
+if /I "%NDKARCH%"=="arm"  set NDKMINABI=3
 
-make -C ..\..\..\luajit HOST_CC="gcc -m32" CROSS="%NDKP%" TARGET_FLAGS="%NDKF%" TARGET=arm TARGET_SYS=Linux HOST_RM=rm TARGET_SONAME=libluajit.so cleaner
-make -C ..\..\..\luajit HOST_CC="gcc -m32" CROSS="%NDKP%" TARGET_FLAGS="%NDKF%" TARGET=arm TARGET_SYS=Linux HOST_RM=rm TARGET_SONAME=libluajit.so BUILDMODE=dynamic amalg %*
+if /I "%NDKARCH%"=="mips" set NDKTRIPLE1=mipsel-linux-android
+if /I "%NDKARCH%"=="mips" set NDKTRIPLE2=mipsel-linux-android
+if /I "%NDKARCH%"=="mips" set NDKMINABI=9
 
-copy ..\..\..\luajit\src\libluajit.so ..\..\bin\Android\arm
-copy ..\..\..\luajit\src\luajit       ..\..\bin\Android\arm
+if /I "%NDKARCH%"=="x86"  set NDKTRIPLE1=x86
+rem if /I "%NDKARCH%"=="x86"  set NDKTRIPLE2=i686-android-linux
+if /I "%NDKARCH%"=="x86"  set NDKTRIPLE2=i686-linux-android
+if /I "%NDKARCH%"=="x86"  set NDKMINABI=9
+
+rem set NDKVER=%NDK%/toolchains/%NDKTRIPLE1%-4.4.3
+set NDKVER=%NDK%/toolchains/%NDKTRIPLE1%-4.6
+set NDKP=%NDKVER%/prebuilt/windows/bin/%NDKTRIPLE2%-
+set NDKABI=%NDKMINABI%
+set NDKF=--sysroot %NDK%/platforms/android-%NDKABI%/arch-%NDKARCH%
+
+echo NDKVER=[%NDKVER%]
+echo NDKP=[%NDKP%]
+echo NDKF=[%NDKF%]
+echo NDKABI=[%NDKABI%]
+
+make -C ..\..\..\luajit HOST_CC="gcc -m32" CROSS="%NDKP%" TARGET_FLAGS="%NDKF%" TARGET=%NDKARCH% TARGET_SYS=Linux HOST_RM=rm TARGET_SONAME=libluajit.so cleaner
+make -C ..\..\..\luajit HOST_CC="gcc -m32" CROSS="%NDKP%" TARGET_FLAGS="%NDKF%" TARGET=%NDKARCH% TARGET_SYS=Linux HOST_RM=rm TARGET_SONAME=libluajit.so BUILDMODE=dynamic amalg
+
+copy ..\..\..\luajit\src\libluajit.so ..\..\bin\android\%NDKARCH%
+copy ..\..\..\luajit\src\luajit       ..\..\bin\android\%NDKARCH%
 
 endlocal
 popd
-
