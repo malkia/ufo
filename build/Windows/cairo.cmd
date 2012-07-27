24ea2e5a38214e013e8e5c35a8410db46a4cb98f
Dimiter 'malkia' Stanev <malkia@gmail.com>
2012-07-26 23:02:22 -0700

    Updated cairo dad69ce4d5568f94621ae60ccdcc683d5bbd0efd (Windows)

--- build/Windows/cairo.cmd
+++ build/Windows/cairo.cmd
@@ -4,7 +4,7 @@ pushd %LB_PROJECT_ROOT%
 setlocal
 
 git clean -fdx
-rem git checkout 1.12.0
+rem git checkout dad69ce4d5568f94621ae60ccdcc683d5bbd0efd
 
 set DEBUG_RELEASE=release
 
@@ -15,11 +15,11 @@ copy /y %LB_ROOT%\bin\Windows\%LB_TARGET_ARCH%\z.lib      %LB_PROJECT_ROOT%\..\z
 copy /y %LB_ROOT%\bin\Windows\%LB_TARGET_ARCH%\png.lib    %LB_PROJECT_ROOT%\..\libpng\libpng.lib
 mkdir %LB_PROJECT_ROOT%\src\GL
 copy /y %~dp0..\include\GL\glext.h %LB_PROJECT_ROOT%\src\GL
-copy %~dp0maglev.c %LB_PROJECT_ROOT%\src
-pushd src && cl %LB_CL_OPTS% -c maglev.c && popd
+rem copy %~dp0maglev.c %LB_PROJECT_ROOT%\src
+rem pushd src && cl %LB_CL_OPTS% -c maglev.c && popd
 for /F %%i in ('cygpath -m %~dp0') do set CYGCD=%%i
 for /F %%i in ('cygpath -m %LB_PROJECT_ROOT%') do set CYGCD2=%%i
-make cairo -f Makefile.win32 CFG=%DEBUG_RELEASE% CC="cl %LB_CL_OPTS% -I%CYGCD%/include -I%CYGCD%/freetype2 -I%CYGCD%/../../../freetype2/include" AR="link /LIB" LD="link %LB_LINK_OPTS% opengl32.lib %CYGCD2%/src/maglev.obj %CYGCD%/../../bin/Windows/%LB_TARGET_ARCH%/freetype2.lib"
+make cairo -f Makefile.win32 CFG=%DEBUG_RELEASE% CC="cl %LB_CL_OPTS% -I%CYGCD%/include -I%CYGCD%/freetype2 -I%CYGCD%/../../../freetype2/include" AR="link /LIB" LD="link %LB_LINK_OPTS% %CYGCD%/../../bin/Windows/%LB_TARGET_ARCH%/freetype2.lib %CYGCD%/../../bin/Windows/%LB_TARGET_ARCH%/regal.lib"
 
 call %~dp0/wdk/install src\%DEBUG_RELEASE%\%LB_PROJECT_NAME%.dll
 call %~dp0/wdk/install src\%DEBUG_RELEASE%\%LB_PROJECT_NAME%.lib
