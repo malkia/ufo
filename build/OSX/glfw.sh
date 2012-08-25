4b58bba0bcf683a95944f2d2a9239620cd322a61
Dimiter "malkia" Stanev <malkia@gmail.com>
2012-08-25 01:52:14 -0700

    glfw osx

--- build/OSX/glfw.sh
+++ build/OSX/glfw.sh
@@ -1,25 +1,22 @@
 set -e
-
 GLFW=../../../glfw
 
 pushd $GLFW
 git clean -fdx
 
 mkdir build
-pushd build
+cd build
+
 # http://stackoverflow.com/questions/1440456/static-libraries-in-version-cross-compiled-program/7835983#7835983
 export MACOSX_DEPLOYMENT_TARGET=10.5
-cmake -D CMAKE_OSX_DEPLOYMENT_TARGET=10.5 -D CMAKE_OSX_ARCHITECTURES="i386;x86_64" -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang -D CMAKE_OSX_SYSROOT="`xcode-select -print-path`/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk" ../
+cmake -D CMAKE_OSX_ARCHITECTURES="i386;x86_64" -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../
 make -j VERBOSE=1
+
+git log -1 >> src/libglfw.3.0.dylib
 popd
 
-git log -1 >> build/src/libglfw.dylib
-popd
-
-mv $GLFW/build/src/libglfw.dylib ../../bin/OSX/glfw.dylib
-
+mv $GLFW/build/src/libglfw.3.0.dylib ../../bin/OSX/glfw.dylib
 install_name_tool -id @loader_path/glfw.dylib ../../bin/OSX/glfw.dylib
 otool -L ../../bin/OSX/glfw.dylib
 file ../../bin/OSX/glfw.dylib
-
 
--- samples/intro.lua
+++ samples/intro.lua
@@ -2,11 +2,24 @@ local ffi = require( "ffi" )
 local sdl = require( "ffi/sdl" )
 local wm = require( "lib/wm/sdl" )
 local uint32ptr = ffi.typeof( "uint32_t*" )
-local cos, sin, abs, sqrt, band, bor, bxor, shl, shr, rol, ror = math.cos, math.sin, math.abs, math.sqrt, bit.band, bit.bor, bit.bxor, bit.lshift, bit.rshift, bit.rol, bit.ror
+local uint8ptr = ffi.typeof( "uint8_t*" )
+local buf = ffi.new( "uint8_t[?]", 4096*4096)
+local cos, sin, abs, sqrt, band, bor, bxor, shl, shr, rol, ror, atan2, random, fmod = math.cos, math.sin, math.abs, math.sqrt, bit.band, bit.bor, bit.bxor, bit.lshift, bit.rshift, bit.rol, bit.ror, math.atan2, math.random, math.fmod
 
 local function intro_shader(x,y,tick,cos,sin)
    local xx, yy, xy = x*x, y*y, x*y
    return bxor(band(tick*xy,tick*yy,tick*xx),xx+y*tick+xx*cos-yy*sin,yy+x*tick+xx*sin+yy*cos)
+end
+
+local function margarita_shader( x, y )
+   local z = sqrt( x*x + y*y ) - 3.5 * atan2(y, x) + sin(x) + cos(y)
+   if z % (3.1415926535898 * 7) < 1.5707963267949 then
+      return 0xFF0000
+   end
+   if z % 3.1415926535898 < 1.5707963267949 then
+      return 0
+   end
+   return 0xFFFFFF
 end
 
 local function render( screen, tick )
@@ -17,16 +30,54 @@ local function render( screen, tick )
     local cos, sin = cos(tick/128), sin(tick/128)
     for i = 0, height-1 do
        for j = 0, width-1 do
-	  pixels_u32[ j + i*pitch ] = intro_shader(j-halfw, i-halfh,tick,cos,sin)
+--	  pixels_u32[ j + i*pitch ] = margarita_shader((j-halfw)*oow*40, (i-halfh)*ooh*40,tick,cos,sin)
+	  pixels_u32[ j + i*pitch ] = intro_shader((j-halfw), (i-halfh),tick,cos,sin)
+       end
+    end
+end
+
+local function jacobi( screen, tick )
+    local width, height, pitch, pitch4 = screen.w, screen.h, screen.pitch, screen.pitch/4
+    local pixels_u32 = ffi.cast( uint32ptr, screen.pixels )
+    local p = ffi.cast( uint8ptr, screen.pixels )
+    local halfw, halfh = width/2, height/2
+    local oow, ooh = 1/width, 1/height
+    local cos, sin = cos(tick/128), sin(tick/128)
+--    ffi.copy(buf, p, height * pitch)
+    buf = p
+    local mask = 0xFEFEFEFE
+    local tick = shr(tick,3)
+    local max = math.max(width,height)
+    local mod = math.abs(fmod(tick, max) - max/2)
+    for i = 1, height-2 do
+       local rnd = random(40)
+       for j = 1, width-2 do
+	  local a = j * 4 + i * pitch
+	  local r = shr(buf[ a+0-pitch ] + buf[ a+0+pitch ] + buf[ a+0-4 ] + buf[ a+0+4] + rnd, 2)
+	  local g = shr(buf[ a+1-pitch ] + buf[ a+1+pitch ] + buf[ a+1-4 ] + buf[ a+1+4] + shr(r,3), 2)
+	  local b = shr(buf[ a+2-pitch ] + buf[ a+2+pitch ] + buf[ a+2-4 ] + buf[ a+2+4] + shr(g,5), 2)
+	  local r = shr(buf[ a+0-pitch ] + buf[ a+0+pitch ] + buf[ a+0-4 ] + buf[ a+0+4] + shr(b,6), 2)
+	  local rgb = bor(shl(b,16),shl(g,8),r)
+	  if i > mod and j > mod and i < height - mod and j < width - mod then
+--	     rgb = shr(
+--		band(rgb,0xFEFEFEFE) + 
+--		band(intro_shader((j-halfw), (i-halfh),tick,cos,sin), 0xFEFEFEFE),
+--		1)
+	     if r > g or g > b then
+		rgb = intro_shader((j-halfw), (i-halfh),tick,cos,sin)
+	     end
+	  end
+	  pixels_u32[ j + i*pitch4 ] = rgb
        end
     end
 end
 
 do
    local prev_time, curr_time, fps = nil, 0, 0, 0, 0
+   local do_jacobi = true
 
-   local ticks_base, ticks = 256 * 128, 0, 0
-   local bounce_mode, bounce_range, bounce_delta, bounce_step = false, 1024, 0, 1
+   local ticks_base, ticks = 256 * 128 * 4, 0, 0
+   local bounce_mode, bounce_range, bounce_delta, bounce_step = false, 1024*4, 0, 1
 
    while wm:update() do
       local event = wm.event
@@ -53,6 +104,14 @@ do
 	 ticks_base = ticks_base + 100
       end
 
+      if wm.kb == ("j"):byte() then
+	 do_jacobi = true
+      end
+
+      if wm.kb == ("k"):byte() then
+	 do_jacobi = false
+      end
+
       if ticks_base > 256 * 256 * 256 then
 	 ticks_base = 0
       end
@@ -68,7 +127,11 @@ do
       end
 
       -- Render the screen, and flip it
-      render( wm.window, ticks + ticks_base )
+      if do_jacobi then
+	 jacobi( wm.window, ticks + ticks_base )
+      else
+	 render( wm.window, ticks + ticks_base )
+      end
 
       -- Calculate the frame rate
       prev_time, curr_time = curr_time, os.clock()
