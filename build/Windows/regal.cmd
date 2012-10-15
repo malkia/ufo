3e0ae409c428845607accaf23b5abe8901676756
Dimiter "malkia" Stanev <malkia@gmail.com>
2012-10-14 18:26:02 -0700

    Updated regal build script and latest binaries for windows

--- build/Windows/regal.cmd
+++ build/Windows/regal.cmd
@@ -5,10 +5,11 @@ setlocal
 pushd %LB_PROJECT_ROOT%
 git clean -fdx
 
-cd win32\vs2010\Regal
+cd build\win32\vs2010\Regal
 set NAME=%LB_PROJECT_NAME%
 
 (for /f "usebackq tokens=3 delims=>=/ " %%i in (`findstr ClCompile %NAME%.vcxproj`) do echo %%i) > sources.tmp
+echo ..\..\..\..\src\md5\src\md5.c >> sources.tmp
 
 cl %LB_CL_OPTS% -Fe%NAME%.dll^
    -D_MSC_VER=1399^
@@ -23,10 +24,13 @@ cl %LB_CL_OPTS% -Fe%NAME%.dll^
    -DREGAL_NO_HTTP=1^
    -DREGAL_NO_GETENV=1^
    -DREGAL_NO_ASSERT=1^
-   -I..\..\..\src\boost^
-   -I..\..\..\src\mongoose^
-   -I..\..\..\include -LD @sources.tmp^
-   /link"%LB_LINK_OPTS% /DEF:..\..\..\src\regal\regal.def ws2_32.lib advapi32.lib gdi32.lib user32.lib"
+   -I..\..\..\..\src\boost^
+   -I..\..\..\..\src\mongoose^
+   -I..\..\..\..\src\md5\include^
+   -I..\..\..\..\src\zlib\include^
+   -I..\..\..\..\src\libpng\include^
+   -I..\..\..\..\include -LD @sources.tmp^
+   /link"%LB_LINK_OPTS% /DEF:..\..\..\..\src\regal\regal.def ws2_32.lib advapi32.lib gdi32.lib user32.lib %LB_ROOT%\bin\Windows\%LB_TARGET_ARCH%\png.lib"
 
 rem Use the Tls API on Windows
 rem -DREGAL_WIN_TLS=1^
