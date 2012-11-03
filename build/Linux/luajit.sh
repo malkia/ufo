7803ea6f574b092a82cc8665da32aad9b1cabb6f
Dimiter 'malkia' Stanev <malkia@bager.(none)>
2012-11-02 23:46:01 -0700

    luajit linux x86

--- build/Linux/luajit.sh
+++ build/Linux/luajit.sh
@@ -3,17 +3,19 @@ ROOT=../../../luajit
 
 
 if [[ `uname -m` == armv* ]]; then
-    make -C $ROOT/src cleaner
+    make -C $ROOT/src clean
     make -C $ROOT/src -j amalg TARGET_SONAME=libluajit.so BUILDMODE=dynamic TARGET_DYNXLDOPTS=-Wl,-rpath,'$$\ORIGIN' Q=
     mv      $ROOT/src/luajit       ../../bin/Linux/arm
     mv      $ROOT/src/libluajit.so ../../bin/Linux/arm
 else
-    make -C $ROOT/src cleaner
+    echo [Making 32-bit]
+    make -C $ROOT/src clean
     make -C $ROOT/src -j amalg TARGET_SONAME=libluajit.so BUILDMODE=dynamic TARGET_DYNXLDOPTS=-Wl,-rpath,'$$\ORIGIN' Q= "CC=gcc -m32"
     mv      $ROOT/src/luajit       ../../bin/Linux/x86
     mv      $ROOT/src/libluajit.so ../../bin/Linux/x86
     
-    make -C $ROOT/src cleaner
+    echo [Making 64-bit]
+    make -C $ROOT/src clean
     make -C $ROOT/src -j amalg TARGET_SONAME=libluajit.so BUILDMODE=dynamic TARGET_DYNXLDOPTS=-Wl,-rpath,'$$\ORIGIN' Q= "CC=gcc -m64"
     mv      $ROOT/src/luajit       ../../bin/Linux/x64
     mv      $ROOT/src/libluajit.so ../../bin/Linux/x64
