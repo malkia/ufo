d8cf05415164762cf207e4aa3a4bad3b0c078477
Dimiter "malkia" Stanev <malkia@gmail.com>
2012-11-12 21:24:16 -0800

    luajit 2.00 x86/x64 binaries for osx targeting 10.5 compiled with xcode 4.6-dp1

--- build/OSX/luajit.sh
+++ build/OSX/luajit.sh
@@ -2,23 +2,22 @@ set -e
 LUAJIT=../../../luajit
 FLAGS='-mmacosx-version-min=10.5 -DLUAJIT_ENABLE_LUA52COMPAT'
 (rm *.tmp 1>/dev/null 2>/dev/null) && true
-make -C $LUAJIT -j BUILDMODE=dynamic CC="clang $FLAGS -arch i386"   LUAJIT_SO=luajit.dylib TARGET_DYLIBPATH=luajit.dylib cleaner
+make -C $LUAJIT -j BUILDMODE=dynamic CC="clang $FLAGS -arch i386"   LUAJIT_SO=luajit.dylib TARGET_DYLIBPATH=luajit.dylib clean
 make -C $LUAJIT -j BUILDMODE=dynamic CC="clang $FLAGS -arch i386"   LUAJIT_SO=luajit.dylib TARGET_DYLIBPATH=luajit.dylib amalg
 mv $LUAJIT/src/luajit luajit32.exe.tmp
 mv $LUAJIT/src/luajit.dylib luajit32.dylib.tmp
-make -C $LUAJIT -j BUILDMODE=dynamic CC="clang $FLAGS -arch x86_64" LUAJIT_SO=luajit.dylib TARGET_DYLIBPATH=luajit.dylib cleaner
+make -C $LUAJIT -j BUILDMODE=dynamic CC="clang $FLAGS -arch x86_64" LUAJIT_SO=luajit.dylib TARGET_DYLIBPATH=luajit.dylib clean
 make -C $LUAJIT -j BUILDMODE=dynamic CC="clang $FLAGS -arch x86_64" LUAJIT_SO=luajit.dylib TARGET_DYLIBPATH=luajit.dylib amalg
 mv $LUAJIT/src/luajit luajit64.exe.tmp
 mv $LUAJIT/src/luajit.dylib luajit64.dylib.tmp
 lipo -create ./luajit*.exe.tmp -output luajit
 lipo -create ./luajit*.dylib.tmp -output luajit.dylib
 # rm *.tmp 1>/dev/null 2>/dev/null
-install_name_tool -id @loader_path/luajit.dylib luajit.dylib
+install_name_tool -id                      @loader_path/luajit.dylib luajit.dylib
 install_name_tool -change luajit.dylib @executable_path/luajit.dylib luajit
 git --git-dir=$LUAJIT/.git log -1 >> luajit
 git --git-dir=$LUAJIT/.git log -1 >> luajit.dylib
-mv luajit ../../bin/OSX
+mv luajit       ../../bin/OSX
 mv luajit.dylib ../../bin/OSX/luajit.dylib
-rm *.tmp
-echo Done
+(rm *.tmp 1>/dev/null 2>/dev/null) && true
 
