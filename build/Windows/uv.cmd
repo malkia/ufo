c9cd497587c920fce12879be50014164673ff42f
Dimiter 'malkia' Stanev <malkia@gmail.com>
2013-01-04 17:17:16 -0800

    windows libuv 926c73cc9176f7b126a839992c1a24dbf728e901

--- build/Windows/uv.cmd
+++ build/Windows/uv.cmd
@@ -10,15 +10,15 @@ set NAME=%LB_PROJECT_NAME%
 echo src/fs-poll.c>sources.tmp
 echo src/inet.c>>sources.tmp
 echo src/uv-common.c>>sources.tmp
-for %%i in (src\ares\*.c) do echo %%i >> sources.tmp
 for %%i in (src\win\*.c) do echo %%i >> sources.tmp
 
-cl %LB_CL_OPTS% -EHsc -Fe%NAME%.dll -LD^
- -DHAVE_CONFIG_H -D_WIN32_WINNT=0x0600 -DEIO_STACKSIZE=262144 -D_GNU_SOURCE -DWIN32 -DNDEBUG^
- -DBUILDING_UV_SHARED^
+REM _set_invalid_parameter_handler is missing in MSVCRT.DLL, and since we are targeting it it's declared as if() doing nothing.
+
+cl %LB_CL_OPTS% -Fe%NAME%.dll -LD^
+ -D_WIN32_WINNT=0x0600 -D_GNU_SOURCE -DNDEBUG -DBUILDING_UV_SHARED^
  -D_set_invalid_parameter_handler=if^
- -Iinclude -Iinclude/uv-private -Isrc -Isrc/ares/config_win32^
- -I"%~dp0\include" @sources.tmp /link"%LB_LINK_OPTS% ws2_32.lib advapi32.lib psapi.lib Iphlpapi.lib"
+ -Iinclude -Iinclude/uv-private^
+ @sources.tmp /link"%LB_LINK_OPTS% ws2_32.lib advapi32.lib psapi.lib iphlpapi.lib"
 
 call %~dp0/wdk/install %LB_PROJECT_NAME%.dll
 call %~dp0/wdk/install %LB_PROJECT_NAME%.lib
